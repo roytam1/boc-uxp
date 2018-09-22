bafb6609fba2b6a2e9f25ed5e59d8a287d077cc4
Matt A. Tobin <email@mattatobin.com>
2018-09-22 17:39:23 -0400

    Remove tests from mailnews

--- mailnews/core/db/gloda/test/unit/test_index_junk_local.js
+++
@@ -1,9 +0,0 @@
-/**
- * Test indexing support for local junk.
- */
-load("base_index_junk.js");
-
-function run_test() {
-  configure_message_injection({mode: "local"});
-  glodaHelperRunTests(tests);
-}
--- mailnews/core/imap/test/unit/head_imap_maildir.js
+++
@@ -1,6 +0,0 @@
-load("head_server.js");
-
-do_print("Running test with maildir");
-
-Services.prefs.setCharPref("mail.serverDefaultStoreContractID",
-                           "@mozilla.org/msgstore/maildirstore;1");
