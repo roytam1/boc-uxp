099f6d3cf70e48da3a88485ed4bdd07b6188c123
Matt A. Tobin <email@mattatobin.com>
2018-09-22 23:46:06 -0400

    Fix jumplist module for change in windows executable name
    THIS SHOULD GET A PREPROCESSED VAR AT SOME POINT

--- mail/components/wintaskbar/windowsJumpLists.js
+++ mail/components/wintaskbar/windowsJumpLists.js
@@ -174,8 +174,8 @@ var WinTaskbarJumpList = {
   _createHandlerAppItem: function WTBJL__createHandlerAppItem(aTask) {
     let file = Services.dirsvc.get("XCurProcD", Ci.nsILocalFile);
 
-    // XXX where can we grab this from in the build? Do we need to?
-    file.append("thunderbird.exe");
+    // XXXTobin: Use a preprocessed var for fuck sakes
+    file.append("interlink.exe");
 
     let handlerApp = Cc["@mozilla.org/uriloader/local-handler-app;1"]
                        .createInstance(Ci.nsILocalHandlerApp);
