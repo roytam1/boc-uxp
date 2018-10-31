42eb98ed6017151e16c9cbed78872e17c2e39bd7
Matt A. Tobin <email@mattatobin.com>
2018-10-30 20:02:16 -0400

    Issue #30 - Remove chat code from UpdateEmailNodeDetails() in msgHdrViewOverlay.js

--- mail/base/content/msgHdrViewOverlay.js
+++ mail/base/content/msgHdrViewOverlay.js
@@ -1278,63 +1278,20 @@ function updateEmailAddressNode(emailAddressNode, address)
 
 function UpdateEmailNodeDetails(aEmailAddress, aDocumentNode, aCardDetails) {
   // If we haven't been given specific details, search for a card.
-  var cardDetails = aCardDetails || GetCardForEmail(aEmailAddress);
+  var cardDetails = aCardDetails ? aCardDetails :
+                                   getCardForEmail(aEmailAddress);
   aDocumentNode.cardDetails = cardDetails;
 
   if (!cardDetails.card) {
     aDocumentNode.setAttribute("hascard", "false");
     aDocumentNode.setAttribute("tooltipstar",
       document.getElementById("addToAddressBookItem").label);
-  } else {
+  }
+  else {
     aDocumentNode.setAttribute("hascard", "true");
     aDocumentNode.setAttribute("tooltipstar",
       document.getElementById("editContactItem").label);
   }
-
-  let chatAddresses = [aEmailAddress];
-  let card = cardDetails.card;
-  if (card) {
-    let gTalk = card.getProperty("_GoogleTalk", null);
-    if (gTalk)
-      chatAddresses.push(gTalk);
-    let jid = card.getProperty("_JabberId", null);
-    if (jid)
-      chatAddresses.push(jid);
-  }
-  let chatContact;
-  if (!("chatHandler" in window)) {
-    window.chatHandler = {};
-    Components.utils.import("resource:///modules/chatHandler.jsm", chatHandler);
-  }
-  let onlineContacts = chatHandler.onlineContacts;
-  for (let chatAddress of chatAddresses) {
-    if (Object.prototype.hasOwnProperty.call(onlineContacts, chatAddresses)) {
-      chatContact = onlineContacts[chatAddress];
-      break;
-    }
-  }
-  if (aDocumentNode.chatContact) {
-    aDocumentNode.chatContact.removeObserver(aDocumentNode.chatContactObserver);
-    delete aDocumentNode.chatContact;
-    delete aDocumentNode.chatContactObserver;
-  }
-  if (chatContact) {
-    aDocumentNode.chatContact = chatContact;
-    aDocumentNode.chatContactObserver = function(aSubject, aTopic, aData) {
-      if (aTopic == "contact-removed") {
-        this.chatContact.removeObserver(this.chatContactObserver);
-        delete this.chatContact;
-        delete this.chatContactObserver;
-        this.removeAttribute("chatStatus");
-        this.removeAttribute("presenceTooltip");
-      }
-      else if (aTopic == "contact-status-changed") {
-        UpdateEmailPresenceDetails(this, this.chatContact);
-      }
-    }.bind(aDocumentNode);
-    chatContact.addObserver(aDocumentNode.chatContactObserver);
-  }
-  UpdateEmailPresenceDetails(aDocumentNode, chatContact);
 
   // When we are adding cards, we don't want to move the display around if the
   // user has clicked on the star, therefore if it is locked, just exit and
@@ -1348,13 +1305,14 @@ function UpdateEmailNodeDetails(aEmailAddress, aDocumentNode, aCardDetails) {
                                       aDocumentNode.cardDetails.card);
 
   if (gShowCondensedEmailAddresses && displayName) {
+    aDocumentNode.setAttribute("label", displayName);
     aDocumentNode.setAttribute("tooltiptext", aEmailAddress);
-  } else {
-    aDocumentNode.removeAttribute("tooltiptext");
-    displayName = aDocumentNode.getAttribute("fullAddress") ||
-                  aDocumentNode.getAttribute("displayName");
   }
-  aDocumentNode.setAttribute("label", displayName);
+  else {
+    aDocumentNode.setAttribute("label",
+      aDocumentNode.getAttribute("fullAddress") ||
+      aDocumentNode.getAttribute("displayName"));
+  }
 }
 
 function UpdateEmailPresenceDetails(aDocumentNode, aChatContact) {
