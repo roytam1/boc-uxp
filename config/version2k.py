febc140a6156ebf9760cf9e4a4920c050a4da927
Matt A. Tobin <email@mattatobin.com>
2018-11-06 03:48:21 -0500

    Update version2k.py to convert number of days since 2000-01-01 to a date

--- config/version2k.py
+++ config/version2k.py
@@ -1,2 +1,10 @@
 from datetime import date
-print (date.today()-date(2000,01,01)).days
+from datetime import timedelta
+import sys
+if len(sys.argv) > 1:
+  try:
+    print date(2000,01,01)+timedelta(days=int(sys.argv[1]))
+  except:
+    print 'Invalid argument'
+else:
+  print (date.today()-date(2000,01,01)).days
