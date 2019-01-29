2d64bbe1f7c0bd07d4beb93867dbc20a0ff45659
Matt A. Tobin <email@mattatobin.com>
2019-01-29 07:50:51 -0500

    [NAVIGATOR] Remove Migration

--- navigator/components/build/nsModule.cpp
+++ navigator/components/build/nsModule.cpp
@@ -5,7 +5,6 @@
 
 #include "mozilla/ModuleUtils.h"
 #include "nsSuiteDirectoryProvider.h"
-#include "nsThunderbirdProfileMigrator.h"
 #include "nsNetCID.h"
 #include "nsRDFCID.h"
 #include "nsFeedSniffer.h"
@@ -18,8 +17,6 @@
 #include "nsGNOMEShellService.h"
 #endif
 
-#define NS_SUITEPROFILEMIGRATOR_CONTRACTID_PREFIX "@mozilla.org/profile/migrator;1?app=suite&type="
-
 /////////////////////////////////////////////////////////////////////////////
 
 #if defined(XP_WIN)
@@ -30,7 +27,6 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(nsMacShellService)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsGNOMEShellService, Init)
 #endif
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsSuiteDirectoryProvider)
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsThunderbirdProfileMigrator)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsFeedSniffer)
 
 #if defined(NS_SUITEWININTEGRATION_CID)
@@ -41,7 +37,6 @@ NS_DEFINE_NAMED_CID(NS_SUITEMACINTEGRATION_CID);
 NS_DEFINE_NAMED_CID(NS_SUITEGNOMEINTEGRATION_CID);
 #endif
 NS_DEFINE_NAMED_CID(NS_SUITEDIRECTORYPROVIDER_CID);
-NS_DEFINE_NAMED_CID(NS_THUNDERBIRDPROFILEMIGRATOR_CID);
 NS_DEFINE_NAMED_CID(NS_FEEDSNIFFER_CID);
 
 /////////////////////////////////////////////////////////////////////////////
@@ -55,7 +50,6 @@ static const mozilla::Module::CIDEntry kSuiteCIDs[] = {
   { &kNS_SUITEGNOMEINTEGRATION_CID, false, NULL, nsGNOMEShellServiceConstructor },
 #endif
   { &kNS_SUITEDIRECTORYPROVIDER_CID, false, NULL, nsSuiteDirectoryProviderConstructor },
-  { &kNS_THUNDERBIRDPROFILEMIGRATOR_CID, false, NULL, nsThunderbirdProfileMigratorConstructor },
   { &kNS_FEEDSNIFFER_CID, false, NULL, nsFeedSnifferConstructor },
   { NULL }
 };
@@ -69,7 +63,6 @@ static const mozilla::Module::ContractIDEntry kSuiteContracts[] = {
   { NS_SUITESHELLSERVICE_CONTRACTID, &kNS_SUITEGNOMEINTEGRATION_CID },
 #endif
   { NS_SUITEDIRECTORYPROVIDER_CONTRACTID, &kNS_SUITEDIRECTORYPROVIDER_CID },
-  { NS_SUITEPROFILEMIGRATOR_CONTRACTID_PREFIX "thunderbird", &kNS_THUNDERBIRDPROFILEMIGRATOR_CID },
   { NS_FEEDSNIFFER_CONTRACTID, &kNS_FEEDSNIFFER_CID },
   { NULL }
 };
