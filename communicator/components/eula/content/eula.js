95c635b94ff0702df2289b59d4e0c640e0822e13
Matt A. Tobin <email@mattatobin.com>
2019-02-27 04:01:50 -0500

    [COMM] Add the basic UI for the eula component

---
+++ communicator/components/eula/content/eula.js
@@ -0,0 +1,20 @@
+Components.utils.import("resource://gre/modules/Communicator.jsm");
+
+function Startup() {
+  main = document.getElementById("main");
+  let textbox = document.createElement("textbox");
+  textbox.setAttribute("id", "eula");
+  textbox.setAttribute("readonly", "true");
+  textbox.setAttribute("multiline", "true");
+  textbox.setAttribute("cols", "80");
+  textbox.setAttribute("rows", "20");
+  textbox.setAttribute("style", "resize: none; font-family: -moz-fixed;");
+  textbox.setAttribute("value",
+                       Communicator.readfile("GreD", "license.txt"));
+  main.appendChild(textbox);
+}
+
+function onAccept() {
+
+}
+
