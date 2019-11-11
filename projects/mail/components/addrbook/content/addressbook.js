e8e8e251558dcd1c75a5d04ffd3f7c6de2a00aeb
Matt A. Tobin <email@mattatobin.com>
2019-11-10 22:35:20 -0500

    Bug 1320475 - Fix broken controllers for cmd_printcard and cmd_printcardpreview.

--- projects/mail/components/addrbook/content/addressbook.js
+++ projects/mail/components/addrbook/content/addressbook.js
@@ -271,6 +271,8 @@ function CommandUpdate_AddressBook()
 {
   goUpdateCommand('cmd_delete');
   goUpdateCommand('button_delete');
+  goUpdateCommand('cmd_printcardpreview');
+  goUpdateCommand('cmd_printcard');
   goUpdateCommand('cmd_properties');
   goUpdateCommand('cmd_newlist');
   goUpdateCommand('cmd_newCard');
