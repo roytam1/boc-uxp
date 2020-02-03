7f7cc5aab4656050db768717f883c9f51017969e
Matt A. Tobin <email@mattatobin.com>
2020-02-03 01:56:38 -0500

    Remove soon to be busted ScratchPad

--- projects/scratchpad/build.mk
+++
@@ -1,7 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-package:
-	cd $(DIST)/bin; \
-  zip -Dr9X ../${ADDON_XPI_NAME}-${ADDON_VERSION}.xpi * -x \*/.mkdir.done; \
